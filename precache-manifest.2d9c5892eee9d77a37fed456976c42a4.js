545e300482ea83efb5341401077013f74d718c57
Thalles Rodrigues <thalles.cezar@gmail.com>
2019-07-13 13:32:52 -0300

    Updates

---
+++ precache-manifest.2d9c5892eee9d77a37fed456976c42a4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4aa8447006d5edb5f6fee501a05193bc",
+    "url": "/visblocks/static/media/line_chart.4aa84470.svg"
+  },
+  {
+    "revision": "1f54507b88864f4ea9ef",
+    "url": "/visblocks/static/css/main.9b0f9afc.chunk.css"
+  },
+  {
+    "revision": "b524c1ab6087caa5c8d8",
+    "url": "/visblocks/static/js/runtime~main.432108c1.js"
+  },
+  {
+    "revision": "bb64a0e9284972853015",
+    "url": "/visblocks/static/js/2.a9259383.chunk.js"
+  },
+  {
+    "revision": "58b0ada6f079240bc35cbe3b9154fd7c",
+    "url": "/visblocks/static/media/bar_chart.58b0ada6.svg"
+  },
+  {
+    "revision": "d510e4c9cc38ee3a324fd5a845f242a3",
+    "url": "/visblocks/static/media/scatter_plot.d510e4c9.svg"
+  },
+  {
+    "revision": "1f54507b88864f4ea9ef",
+    "url": "/visblocks/static/js/main.5450b859.chunk.js"
+  },
+  {
+    "revision": "7a5a745381badedc2af1f1fd7aaedc0b",
+    "url": "/visblocks/static/media/map_pin.7a5a7453.svg"
+  },
+  {
+    "revision": "76354149ba029caae5f52600bc787363",
+    "url": "/visblocks/static/media/data.76354149.svg"
+  },
+  {
+    "revision": "de925beda0fc8196585c9a63f4998efa",
+    "url": "/visblocks/static/media/table.de925bed.svg"
+  },
+  {
+    "revision": "a546b0e27ebd5ce03718eb5fbb52c455",
+    "url": "/visblocks/static/media/pie_chart.a546b0e2.svg"
+  },
+  {
+    "revision": "205a8e5cc2eb4b419317633239b90eb9",
+    "url": "/visblocks/index.html"
+  }
+];
